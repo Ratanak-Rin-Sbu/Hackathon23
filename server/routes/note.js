4ebb44feb38317558559340b780a1c74703cd3eb
Yunah Kim <yunah.kim@stonybrook.edu>
2023-05-13 05:55:34 +0900

    edit

--- server/routes/note.js
+++ server/routes/note.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require("passport");
 const mongoose = require("mongoose");
 const Note = require("../model/Notes");
+const User = require("../model/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = "jwt secret string";
@@ -12,11 +13,23 @@ router.get("/", (req, res) => {
   console.log("note route running");
 });
 
+router.get(
+  "/getNotes",
+  passport.authenticate("jwt", { session: false }),
+  wrapAsync(async function (req, res) {
+    console.log("getting notes of", req.user, req.user._id);
+    const user = await User.findById(req.user._id);
+    const classes = user.classes;
+    console.log(classes);
+    res.send({ classes });
+  })
+);
+
 router.post(
   "/postNotes",
   passport.authenticate("jwt", { session: false }),
   wrapAsync(async function (req, res) {
-    console.log(req.user.name);
+    console.log(req.user.name, req.user._id);
     console.log("Posted with body: " + JSON.stringify(req.body));
     const newNote = new Note({
       userName: req.user.name,
