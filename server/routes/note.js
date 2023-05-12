579a77b15a07d0caafb46d7abb6a50e7174d503d
Yunah Kim <yunah.kim@stonybrook.edu>
2023-05-13 03:48:28 +0900

    fix note API

--- server/routes/note.js
+++ server/routes/note.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const mongoose = require("mongoose");
-const Note = require("../model/User");
+const Note = require("../model/Notes");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = "jwt secret string";
@@ -14,77 +14,18 @@ router.get("/", (req, res) => {
 
 router.post(
   "/postNotes",
+  passport.authenticate("jwt", { session: false }),
   wrapAsync(async function (req, res) {
+    console.log(req.user.name);
     console.log("Posted with body: " + JSON.stringify(req.body));
     const newNote = new Note({
-      userName: req.body.userName,
+      userName: req.user.name,
       className: req.body.className,
       createdDate: Date.now(),
       details: req.body.details,
     });
-
     await newNote.save();
-    res.json(newQuestion);
+    res.json(newNote);
   })
 );
-// router.get(
-//   "/getTransactions",
-//   passport.authenticate("jwt", { session: false }),
-//   async (req, res) => {
-//     // console.log("getting transaction of", req.user, req.user._id);
-//     const transactions = await Transaction.find({ user: req.user._id }).sort({
-//       createdAt: -1,
-//     });
-//     //find with an empth body means that it will be looking for everything
-//     res.send(transactions);
-//   }
-// );
-// router.get(
-//   "/getGroupedTransactions",
-//   passport.authenticate("jwt", { session: false }),
-//   async (req, res) => {
-//     // console.log("getting transaction of", req.user, req.user._id);
-//     const transactions = await Transaction.find({ user: req.user._id }).sort({
-//       createdAt: -1,
-//     });
-//     const test = await Transaction.aggregate([
-//       { $match: { user: req.user._id } },
-//       {
-//         $group: {
-//           _id: { $month: "$date" },
-//           transactions: {
-//             $push: {
-//               amount: "$amount",
-//               details: "$details",
-//               date: "$date",
-//             },
-//           },
-//           totalExpenses: { $sum: "$amount" },
-//         },
-//       },
-//     ]);
-//     //find with an empth body means that it will be looking for everything
-//     res.send(test);
-//   }
-// );
-
-// router.put(
-//   "/updateTransaction/:id",
-//   passport.authenticate("jwt", { session: false }),
-//   async (req, res) => {
-//     const updatedData = req.body;
-//     await Transaction.updateOne({ _id: req.params.id }, { $set: updatedData });
-//     res.json({ message: "Successfully updated on DB" });
-//   }
-// );
-
-// router.delete(
-//   "/delete/:id",
-//   passport.authenticate("jwt", { session: false }),
-//   async (req, res) => {
-//     const _id = req.params.id;
-//     await Transaction.findOneAndDelete({ _id: _id });
-//     res.json({ message: "Successfully deleted from DB" });
-//   }
-// );
 module.exports = router;
